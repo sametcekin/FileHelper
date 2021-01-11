d3587be2bd6981bc4336bf4d2b6368a1ea28b095
sametcekin <sametcekin@users.noreply.github.com>
2021-01-11 14:43:37 +0300

    Add project files.

---
+++ FileHelper/FileFolderCreator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FileHelper
+{
+    public static class FileFolderCreator
+    {
+        /// <summary>
+        /// If folder does not exist then create, also files.
+        /// </summary>
+        /// <param name="pathFolder">For folder path</param>
+        /// <param name="pathFile">For array path file</param>
+        public static void FolderFileCreate(string pathFolder, string[] pathFile)
+        {
+            if (!Directory.Exists(pathFolder))
+            {
+                Directory.CreateDirectory(pathFolder);
+            }
+            foreach (var file in pathFile)
+            {
+                if (!File.Exists(file))
+                {
+                    using StreamWriter sw = File.CreateText(file);
+                }
+            }
+        }
+
+    }
+}
